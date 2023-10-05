65d10f12340c7d7c9469cb8db08f86db8f8cbec4
Matias Mollecker <68872715+MatiasMollecker@users.noreply.github.com>
2023-10-04 22:57:43 -0300

    Update Chanel_AWGN.m

--- Comunicaciones_Digitales/Simulador/src/Chanel_AWGN.m
+++ Comunicaciones_Digitales/Simulador/src/Chanel_AWGN.m
@@ -6,12 +6,8 @@ function [o_data_s] = Chanel_AWGN(i_config_s,s)
 
     config.EbNo_db = 5; 
     config.M = 4;   % Cantidad de niveles de la modulacion
-    config.NOS = 2;
-    config.ISE=0;
-    config.firorder=1;
-    config.fc=20e9;
-    config.BR=32e9;
-    config.rolloff=0.5;
+    config.NOS = 2;  % Cantidad de niveles de la modulacion
+    
     %--------------------------%
     %       REASSIGNMENT
     %--------------------------%
@@ -32,15 +28,6 @@ function [o_data_s] = Chanel_AWGN(i_config_s,s)
     EbNo_db = config.EbNo_db;
     M = config.M;
     NOS = config.NOS;
-    ISE = config.ISE;
-    BR=config.BR;
-    fc= config.fc;
-    firorder= config.firorder;
-    rolloff=config.rolloff;
-    
-    f=(BR+rolloff*BR)/2*NOS;
-    fc=fc/f;
-    
     %--------------------------%
     %         PROCESS
     %--------------------------%
@@ -59,12 +46,14 @@ Pn = Ps/SNR_ch;
 % Noise generator
 n = sqrt(Pn/2) .* (randn(length(s),1) + 1j.*randn(length(s),1));
 
+% --  influye si se aniade el ruido antes o despues de filtrar  --
+% si se añade ruido despues de filtrar, se debe calcular la potencia
+% despues de filtrar 
+
 % Noise addition
-rx = s + n; 
-if ISE
-        b=fir1(firorder,fc);
-    else
-        b=1;
+rx = s + n;
+o_data_s.yup_n=rx; 
+
 end
     
 rx = filter(b,1,rx);
